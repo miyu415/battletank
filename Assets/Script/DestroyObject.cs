640238fcdf53eead6e8f514d4f17c6fcd9397ea0
miyu415 <kren415@outlook.jp>
2021-04-06 14:21:18 +0900

    手順５

---
+++ Assets/Script/DestroyObject.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObject : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject effectPrefab;
+    [SerializeField]
+    private GameObject effectPrefab2;
+    public int objectHP;
+    //このメゾットはコライダー同士がぶつかった瞬間に呼び出してる
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("shell"))
+        {
+            objectHP -= 1;
+            if (objectHP > 0)
+            {
+                Destroy(other.gameObject);
+                GameObject effect = Instantiate(effectPrefab2, transform.position, Quaternion.identity);
+                Destroy(effect, 2.0f);
+            }
+            else
+            {
+                Destroy(other.gameObject);
+                GameObject effect2 = Instantiate(effectPrefab2, transform.position, Quaternion.identity);
+                Destroy(effect2, 2.0f);
+                Destroy(this.gameObject);
+            } 
+        }
+    }
+}
---
+++ Assets/Script/ShotShell.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotShell : MonoBehaviour
+{
+    public float shotSpeed;
+
+    //privateの状態でもinspector上から設定できるようにするテクニック。
+    [SerializeField]
+    private GameObject shellPrefab;
+
+    [SerializeField]
+    private AudioClip shotSound;
+    
+    void Update()
+    {
+        //もしもマウスの左ボタンを押したら
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+           //砲弾のプレハブを実体化する
+            GameObject shell = Instantiate(shellPrefab, transform.position, Quaternion.identity);
+            //砲弾に付いてるRigidbodyにアクセスする
+            Rigidbody shellRb = shell.GetComponent<Rigidbody>();
+            //forward（z軸）の方向に力を加える
+            shellRb.AddForce(transform.forward * shotSpeed);
+            //発射した砲弾を3秒後に破壊する
+            Destroy(shell, 3.0f);
+            //砲弾の発射音をだす
+            AudioSource.PlayClipAtPoint(shotSound, transform.position);
+        }
+    }
+}
