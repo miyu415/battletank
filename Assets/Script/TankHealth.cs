886857efcb313677d56f87d7958d69265140becc
miyu415 <kren415@outlook.jp>
2021-04-10 16:07:21 +0900

    手順１０

--- Assets/Script/TankHealth.cs
+++ Assets/Script/TankHealth.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class TankHealth : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private GameObject effectPrefab1;
+    [SerializeField]
+    private GameObject effectPrefab2;
+    public int tankHP;
+    private void OnTriggerEnter(Collider other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //もしぶつっかてきた相手のTagが”EnemyShell”であったならば
+        if(other.gameObject.tag=="EnemyShell")
+        {
+            //HPを１ずつ減少させる
+            tankHP -= 1;
+            //ぶつっかてきた相手側を破壊する
+            Destroy(other.gameObject);
+            if(tankHP>0)
+            {
+                GameObject effect1 = Instantiate(effectPrefab1,transform.position, Quaternion.identity);
+                Destroy(effect1, 1.0f);
+            }
+            else
+            {
+                GameObject effect2 = Instantiate(effectPrefab1, transform.position, Quaternion.identity);
+                Destroy(effect2, 1.0f);
+                Destroy(gameObject);
+            }
+        }
     }
 }
