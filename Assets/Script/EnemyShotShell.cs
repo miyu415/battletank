3663bf318a85c6f86f6c8ea4d50c45b7a0410af9
miyu415 <kren415@outlook.jp>
2021-05-26 11:01:57 +0900

    照準器追加

--- Assets/Script/EnemyShotShell.cs
+++ Assets/Script/EnemyShotShell.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyShotShell : MonoBehaviour
 {
@@ -10,14 +11,22 @@ public class EnemyShotShell : MonoBehaviour
     [SerializeField]
     private AudioClip shotSound;
     private int interval;
+    public float stopTimer = 5.0f;
+    [SerializeField]
+    private Text stopLabel;
     // Update is called once per frame
     void Update()
     {
         interval += 1;
-
-        if(interval%60==0)//余りが０の時
+        stopTimer -= Time.deltaTime;
+        if (stopTimer < 0)
         {
-            GameObject enemyShell = Instantiate(enemyShellPrefab, transform.position,Quaternion.identity);//enemyShellにInstantiateメソッドを代入してる、指定は不要
+            stopTimer = 0;
+        }
+        stopLabel.text = "" + stopTimer.ToString("0");
+        if (interval % 60 == 0)//余りが０の時
+        {
+            GameObject enemyShell = Instantiate(enemyShellPrefab, transform.position, Quaternion.identity);//enemyShellにInstantiateメソッドを代入してる、指定は不要
             Rigidbody enemyShellRb = enemyShell.GetComponent<Rigidbody>();//rbをコンポーネントしている
             enemyShellRb = enemyShell.GetComponent<Rigidbody>();//.GetComponentを実行する対象となるGameObject型の指定が必要、＝の右側にあるメソッドは戻り値がある
             enemyShellRb.AddForce(transform.forward * shotSpeed);//戻り値なし
@@ -25,4 +34,11 @@ public class EnemyShotShell : MonoBehaviour
             Destroy(enemyShell, 3.0f);
         }
     }
+
+        public void AddStopTimer(float amout)
+        {
+            stopTimer += amout;
+            stopLabel.text = "" + stopTimer.ToString("0");
+        }
+    
 }
